817b14a46786b0734c412deff2df50d787711ba3
wenkeyu <kywen1119@163.com>
2021-09-07 23:25:39 +0800

    'mlm'

---
+++ train_mlm.py
@@ -0,0 +1,97 @@
+import logging
+import os
+from pprint import pprint
+
+import tensorflow as tf
+
+from config import parser
+from data_helper_mlm import create_datasets
+from metrics import Recorder
+from model_mlm import BERTforMaskedLM
+from util import test_spearmanr
+
+
+def train(args):
+    # 1. create dataset and set num_labels to args
+    train_dataset, val_dataset = create_datasets(args)
+    # 2. build model
+    model = BERTforMaskedLM(args)
+    # 3. save checkpoints
+    checkpoint = tf.train.Checkpoint(model=model, step=tf.Variable(0))
+    checkpoint_manager = tf.train.CheckpointManager(checkpoint, args.savedmodel_path, args.max_to_keep)
+    checkpoint.restore(checkpoint_manager.latest_checkpoint)
+    if checkpoint_manager.latest_checkpoint:
+        logging.info("Restored from {}".format(checkpoint_manager.latest_checkpoint))
+    else:
+        logging.info("Initializing from scratch.")
+    # 4. create loss_object and recorders
+    loss_object = tf.keras.losses.BinaryCrossentropy(reduction=tf.keras.losses.Reduction.NONE)
+    train_recorder, val_recorder = Recorder(), Recorder()
+
+    # 5. define train and valid step function
+    @tf.function
+    def train_step(inputs):
+        labels = inputs['mask_labels']
+        with tf.GradientTape() as tape:
+            predictions, loss = model(inputs, training=True)
+            # loss = loss_object(labels, predictions) * labels.shape[-1]  # convert mean back to sum
+        gradients = tape.gradient(loss, model.get_variables())
+        model.optimize(gradients)
+        train_recorder.record(loss, labels, predictions)
+
+    @tf.function
+    def val_step(inputs):
+        vids = inputs['vid']
+        labels = inputs['mask_labels']
+        predictions, loss = model(inputs, training=False)
+        # loss = loss_object(labels, predictions) * labels.shape[-1]  # convert mean back to sum
+        val_recorder.record(loss, labels, predictions)
+        return vids, loss
+
+    # 6. training
+    for epoch in range(args.start_epoch, args.epochs):
+        for train_batch in train_dataset:
+            checkpoint.step.assign_add(1)
+            step = checkpoint.step.numpy()
+            if step > args.total_steps:
+                break
+            train_step(train_batch)
+            if step % args.print_freq == 0:
+                train_recorder.log(epoch, step)
+                train_recorder.reset()
+
+            # 7. validation
+            if step % args.eval_freq == 0:
+                vid_loss = {}
+                for val_batch in val_dataset:
+                    vids, losses = val_step(val_batch)
+                    for vid, loss in zip(vids.numpy(), losses.numpy()):
+                        vid = vid.decode('utf-8')
+                        vid_loss[vid] = loss
+                # 8. test spearman correlation
+                # spearmanr = test_spearmanr(vid_embedding, args.annotation_file)
+                # val_recorder.log(epoch, step, prefix='Validation result is: ', suffix=f', spearmanr {spearmanr:.4f}')
+                val_recorder.log(epoch, step)
+                val_recorder.reset()
+
+                # 9. save checkpoints
+                # if spearmanr > 0.45:
+                checkpoint_manager.save(checkpoint_number=step)
+    checkpoint_manager.save(checkpoint_number=step)
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.savedmodel_path):
+        os.makedirs(args.savedmodel_path)
+
+    pprint(vars(args))
+    train(args)
+
+
+if __name__ == '__main__':
+    main()
